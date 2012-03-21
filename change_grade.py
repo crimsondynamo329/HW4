15822a8a4b5dca0f6a73ba21ddcd89ddd41cdd80
Alex Mosolgo-Clark <crimsondynamo329@gmail.com>
2012-03-20 20:44:34 -0400

    Added sample input file and updated change_grade.py to use a function.

--- change_grade.py
+++ change_grade.py
@@ -5,14 +5,17 @@ import fileinput
 from sys import stderr,stdout,argv,exit
 
 if len(argv) != 3:
-    stderr.write("Usage: myprog.py STRING GRADE\n")
+    stderr.write("Usage: myprog.py STRING GRADE <inputfile\n")
     exit(1)
     
-for line in fileinput.input('-'):
-    m = re.search(argv[1], line)
-    values = line.rstrip().split(",")
-    if m:
-        values[3] = argv[2]
-        print ",".join(values)
-    else:
-        print line,
+changeGrade(argv[1], argv[2])
+    
+def changeGrade( searchString, newGrade ):
+    for line in fileinput.input('-'):
+        m = re.search(searchString, line)
+        values = line.rstrip().split(",")
+        if m:
+            values[3] = newGrade
+            print ",".join(values)
+        else:
+            print line,
