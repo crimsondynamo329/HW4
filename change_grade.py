025a23c3949b031e27b3637cc37d8a3580489c83
Alex Mosolgo-Clark <crimsondynamo329@gmail.com>
2012-03-20 20:52:25 -0400

    Updated change_grade.py with a __main__ function.

--- change_grade.py
+++ change_grade.py
@@ -3,12 +3,6 @@
 import re
 import fileinput
 from sys import stderr,stdout,argv,exit
-
-if len(argv) != 3:
-    stderr.write("Usage: myprog.py STRING GRADE <inputfile\n")
-    exit(1)
-    
-changeGrade(argv[1], argv[2])
     
 def changeGrade( searchString, newGrade ):
     for line in fileinput.input('-'):
@@ -19,3 +13,14 @@ def changeGrade( searchString, newGrade ):
             print ",".join(values)
         else:
             print line,
+            
+def __main__():
+    if len(argv) != 3:
+        stderr.write("Usage: myprog.py STRING GRADE <inputfile\n")
+        exit(1)
+    changeGrade(argv[1], argv[2])
+
+if len(argv) != 3:
+    stderr.write("Usage: myprog.py STRING GRADE <inputfile\n")
+    exit(1)
+changeGrade(argv[1], argv[2])
