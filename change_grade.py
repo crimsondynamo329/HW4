be24d49fb13fbb7b3fe33b727fd301a44241b3f6
Alex Mosolgo-Clark <crimsondynamo329@gmail.com>
2012-03-20 21:01:35 -0400

    Updated change_grade.py to use if __name__=='__main__' structure (so it can be used as a script and also imported).

--- change_grade.py
+++ change_grade.py
@@ -2,7 +2,6 @@
 
 import re
 import fileinput
-from sys import stderr,stdout,argv,exit
     
 def changeGrade( searchString, newGrade ):
     for line in fileinput.input('-'):
@@ -14,13 +13,9 @@ def changeGrade( searchString, newGrade ):
         else:
             print line,
             
-def __main__():
+if __name__=='__main__':
+    from sys import stderr,stdout,argv,exit
     if len(argv) != 3:
         stderr.write("Usage: myprog.py STRING GRADE <inputfile\n")
         exit(1)
     changeGrade(argv[1], argv[2])
-
-if len(argv) != 3:
-    stderr.write("Usage: myprog.py STRING GRADE <inputfile\n")
-    exit(1)
-changeGrade(argv[1], argv[2])
